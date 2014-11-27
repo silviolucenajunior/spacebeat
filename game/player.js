ba5df3260821227846f31435e68842d8e1de501a
Silvio Lucena Junior <silvio@intrapoint.com>
2014-11-27 17:33:18 -0300

    updates on controlls

--- game/player.js
+++ game/player.js
@@ -7,6 +7,7 @@ var player = {
     * BEGIN PLAYER PROPERTIES *
     ***************************/
    fireDelay: 200, //Delay between fires
+   fireLevel : 1,
    energy: 100, //Current Energy of player. Energy is used by player to do actions
    health: 100, //Current Health of player. If Health 0 player die and lost life
    lifes : 3, //Lifes remains before game over
@@ -17,6 +18,7 @@ var player = {
       SHIELD : 5
    },
    energyReload : 2, //Energy has a amount reloaded by sec
+
    /************************
    * END PLAYER PROPERTIES *
    *************************/
@@ -28,15 +30,33 @@ var player = {
       game.load.image('player', 'assets/spaceship.png');
    },
 
+   _commandMap: {
+      '__hiperspeed__' : function(){
+         _self.speed = 6;
+      },
+      '__left__' : function(){
+         _self.x += _self.speed;
+      },
+      '__right__' : function(){
+         _self.x -= _self.speed;
+      },
+      '__up__' : function(){
+         _self.y -= _self.speed;
+      },
+      '__down__' : function(){
+         _self.y += _self.speed;
+      }
+   },
+
    create: function(){
       console.log("Player Create");
       _self = game.add.sprite(320, 240, 'player');
    },
 
    update: function(){
-      var speed = 4;
+      _self.speed = 4;
       if (game.input.keyboard.isDown(Phaser.Keyboard.SHIFT)){
-         speed = 6;
+         _self.speed = 6;
       }
 
       if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
@@ -50,6 +70,73 @@ var player = {
       } else if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
          _self.y += speed;
       }
+   },
+   /**
+    * Map input key to actions keys on object
+    */
+   _inputMap: function(key){
+      switch (key){
+         case "__hiperspeed__" : {
+            break;
+         }
+         case "__right__" : {
+            break;
+         }
+         case "__left__" : {
+            break;
+         }
+         case "__up__" : {
+            break;
+         }
+         case "__down__" : {
+            break;
+         }
+         case "__action1__" : {
+            break;
+         }
+         case "__action2__" : {
+            break;
+         }
+         case "__action3__" : {
+            break;
+         }
+         case "__action4__" : {
+            break;
+         }
+      }
+   },
+   _createBullet: function(position, damage){
+
+   },
+   _createShild: function(){
+
+   },
+   _createBomb: function(){
+
+   },
+   _createBean: function(){
+
+   },
+   _startChargeBean: function(){
+
+   },
+   _releaseChargeBean: function(){
+
+   },
+   rapidFire: function(){
+      if (lastFire - date.Now() > fireDelay){
+         if (fireLevel === 1) {
+            _createBullet(__MIDDLE__, 1);
+         } else if (fireLevel === 2){
+            _createBullet(__LEFT__, 1);
+            _createBullet(__RIGHT__, 1);
+         } else if (fireLevel === 3){
+            _createBullet(__LEFT__, 1);
+            _createBullet(__RIGHT__, 1);
+            _createBullet(__MIDDLE__, 1);
+         }
+      }
    }
+
 }
 
